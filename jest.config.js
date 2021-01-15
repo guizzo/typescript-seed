16a69a421dec7075bd770535275c1928a686a7bc
Simone Gizzi <simonegizzi85@gmail.com>
2021-01-15 15:37:36 +0100

    updated scaffold

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json"
+    }
+  },
+  moduleFileExtensions: [
+    "ts",
+    "js"
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  testEnvironment: "node"
+};
